508df8a8a8009526de118a09d5f092ec3fbfa37a
Choo Jia Wei <jiawei.choo.2018@sis.smu.edu.sg>
2020-03-12 22:11:08 +0800

    Initial commit
    Initial commit

---
+++ login.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+session_start();
+if (isset($_SESSION["staff_id"])) {
+    header("location:staff/admin.php");
+}
+?>
+<head>
+    <title>Staff Login</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width">
+    <!--[if lt IE 9]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <!-- Bootstrap libraries -->
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+          crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+    <!--===============================================================================================-->
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!--===============================================================================================-->
+    <!-- Latest compiled and minified JavaScript -->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script
+            src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <script
+            src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+            integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+            crossorigin="anonymous"></script>
+
+    <script
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+            integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+</head>
+<script>
+    $(document).ready(function () {
+        $("#loadingwheel").hide();
+        $("#error_msg").hide();
+        $("#submit").click(function () {
+            $("#loadingwheel").show();
+            let email = $("#email").val();
+            let password = $("#password").val();
+            let serviceURL = "http://127.0.0.1:8001/staff/login/" + email;
+            // anonymous async function
+            // - using await requires the function that calls it to be async
+            $(async () => {
+                try {
+                    const response =
+                        await fetch(
+                            serviceURL, {
+                                method: 'POST',
+                                headers: {"Content-Type": "application/json"},
+                                body: JSON.stringify({password: password})
+                            }
+                        );
+                    const data = await response.json();
+                    if (data.result == true) {
+                        $.post("session_staff.php", {"staff_id": data.staff_id});
+                        location.reload();
+                    }else{
+                        $("#loadingwheel").hide();
+                        $("#error_msg").show();
+                    }
+                } catch (e) {
+                    $("#loadingwheel").hide();
+                    $("#error_msg").show();
+                }
+
+            });
+
+        });
+    });
+
+
+</script>
+<body>
+
+<div class="limiter">
+    <div class="container-login100">
+        <div class="wrap-login100 p-l-85 p-r-85 p-t-55 p-b-55">
+            <form class="login100-form validate-form flex-sb flex-w">
+					<span class="login100-form-title p-b-32">
+						Account Login
+					</span>
+
+                <span class="txt1 p-b-11">
+						Email
+					</span>
+                <div class="wrap-input100 validate-input m-b-36" data-validate="Email is Required">
+                    <input class="input100" type="email" name="email" id="email">
+                    <span class="focus-input100"></span>
+                </div>
+
+                <span class="txt1 p-b-11">
+						Password
+					</span>
+                <div class="wrap-input100 validate-input m-b-12" data-validate="Password is required">
+						<span class="btn-show-pass">
+							<i class="fa fa-eye"></i>
+						</span>
+                    <input class="input100" type="password" name="pass" id="password">
+                    <span class="focus-input100"></span>
+                </div>
+
+                <div class="flex-sb-m w-full p-b-48">
+                    <div class="contact100-form-checkbox">
+                        <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+                        <label class="label-checkbox100" for="ckb1">
+                            Remember me
+                        </label>
+                    </div>
+
+                    <div>
+                        <a href="#" class="txt3">
+                            Forgot Password?
+                        </a>
+                    </div>
+                </div>
+                <div class="validation-login" id="error_msg">
+                    <P style="color: #c80000;text-align: center;" id="msg_error">The password or email provided is
+                        incorrect.</P>
+                </div>
+                <br/>
+                <div class="container-login100-form-btn">
+
+                    <input type="button" class="login100-form-btn" id="submit" value="Login"/>
+
+                </div>
+
+            </form>
+
+        </div>
+    </div>
+</div>
+<div class="loading style-2" id="loadingwheel">
+    <div class="loading-wheel"></div>
+</div>
+<div id="dropDownSelect1"></div>
+
+
+<!--===============================================================================================-->
+<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+
+<!--===============================================================================================-->
+<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/daterangepicker/moment.min.js"></script>
+<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+<script src="js/main.js"></script>
+
+</body>
+</html>
