fab82b6f3d880b01c82e178c20663c064f592fe8
Choo Jia Wei <jiawei.choo.2018@sis.smu.edu.sg>
2020-03-12 23:33:17 +0800

    Edited Dashboard

--- session_staff.php
+++ session_staff.php
@@ -4,10 +4,10 @@ $_SESSION['staff_id'] = $_POST['staff_id'];
 $_SESSION['first_name'] = $_POST['first_name'];
 $_SESSION['last_name'] = $_POST['last_name'];
 $_SESSION['prefix'] = $_POST['prefix'];
-if(isset($_POST['middle_name'])){
+if (isset($_POST['middle_name'])) {
     $_SESSION['middle_name'] = $_POST['middle_name'];
 }
-if(isset($_POST['suffix'])){
-    $_SESSION = $_POST['suffix'];
+if (isset($_POST['suffix'])) {
+    $_SESSION['suffix'] = $_POST['suffix'];
 }
 ?>
